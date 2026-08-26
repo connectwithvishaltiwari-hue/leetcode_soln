208ed4e8e3c98d6362fe2b2e24309cb390a82cb6
Vishal Tiwari <connectwithvishaltiwari@gmail.com>
2026-08-27 01:53:02 +0530

    Time: 7 ms (51.18%), Space: 13.6 MB (6.76%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -11,6 +11,19 @@ public:
             }
         }
         vow_max = max;
+        // for (int i = 0; i < n-k; i++){
+        //     if (s1[0] == 'a' || s1[0] == 'e' || s1[0] == 'i' || s1[0] == 'o' || s1[0] == 'u'){
+        //         max--;
+        //     }s1 = s.substr(i+1,i+k+1);
+        //     if (s1[k-1] == 'a' || s1[k-1] == 'e' || s1[k-1] == 'i' || s1[k-1] == 'o' || s1[k-1] == 'u'){
+        //         max++;
+        //     }
+        //     if (vow_max < max){
+        //         vow_max = max;
+        //     }
+        // }return vow_max;
+
+        // sliding window
         for (int i = k; i < n; i++){
             if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'){
                 max++;
