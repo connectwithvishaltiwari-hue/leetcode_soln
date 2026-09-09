276e537f7003ad09cc0f741a7e55e2f86de77368
Vishal Tiwari <connectwithvishaltiwari@gmail.com>
2026-09-09 10:54:40 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.05%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int a = 0;
+        for (int i = 0; i < s.size() - 1; i++){
+            a += abs(s[i] - s[i+1]);
+        }return a;
+    }
+};
