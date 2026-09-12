695ba9c5904586cc18c2eb6e1c3b7b537d9d4f4f
Vishal Tiwari <connectwithvishaltiwari@gmail.com>
2026-09-13 01:30:03 +0530

    Time: 0 ms (100%), Space: 13.6 MB (77.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,10 +3,8 @@ public:
     int searchInsert(vector<int>& arr, int target) {
         int l = 0;
         int r = arr.size() - 1;
-        int idx = 0;
         while(l<=r){
             int mid = l + (r-l)/2;
-            idx = mid;
             if(arr[mid] == target){
                 return mid;
             }else if(arr[mid] < target){
